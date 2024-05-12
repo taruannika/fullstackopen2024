e0f06b5673221683fdebe73d6b8de395b3852f92
HamalainenTaru <thamalainen1990@gmail.com>
2024-05-12 22:15:44 +0300

    part1 task 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -28,12 +28,18 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>Statistics</h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {all}</p>
-      <p>Average {average}</p>
-      <p>Positive {positive} %</p>
+      {all === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <p>Good {good}</p>
+          <p>Neutral {neutral}</p>
+          <p>Bad {bad}</p>
+          <p>All {all}</p>
+          <p>Average {average}</p>
+          <p>Positive {positive} %</p>
+        </>
+      )}
     </div>
   );
 };

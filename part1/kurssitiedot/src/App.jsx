29c5ac045fdba48a4573edc6729b54c62e65b8ce
HamalainenTaru <thamalainen1990@gmail.com>
2024-05-12 21:26:53 +0300

    part1 task 1.5

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -1,31 +1,34 @@
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
 
 const Header = ({ course }) => {
-  return <h1>{course}</h1>;
+  const { name } = course;
+  return <h1>{name}</h1>;
 };
 
 const Content = ({ parts }) => {
